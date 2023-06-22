185726ef5fafae46283310006f788f337c2bc27c
TKOTKCh <TKOTKCh@163.com>
2023-06-22 20:37:11 +0800

    modify getQueue and getRecord

--- springboot/src/main/java/com/wen/entity/Record.java
+++ springboot/src/main/java/com/wen/entity/Record.java
@@ -3,12 +3,15 @@ package com.wen.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@TableName("borrow_records")//mybatis-plus
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Record {
     @TableId(type = IdType.AUTO)
     private Integer id;
@@ -23,5 +26,7 @@ public class Record {
     private String comment;
     private Integer rating;
     private String title;//书名
+    private String user_name;//用户名
+    private String phone;//手机号
 
 }
